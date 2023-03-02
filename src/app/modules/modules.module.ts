7c75165df8f30e415476e44b5df9d63814116251
Ivan <alum.iescalantes@iesalixar.org>
2023-03-02 16:37:38 +0100

    Unalobaenelarmario

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { BotonComponent } from './boton/boton.component';
 
 
 
 @NgModule({
   declarations: [
     RegistrationComponent,
-    HomeComponent
+    HomeComponent,
+    BotonComponent
   ],
   imports: [
     CommonModule,
