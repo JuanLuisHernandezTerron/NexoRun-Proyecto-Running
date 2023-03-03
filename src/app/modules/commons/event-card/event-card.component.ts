f0471bc86a0092e055538b712abec86ad4514918
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2023-03-03 19:28:55 +0100

    arreglo

--- src/app/modules/commons/event-card/event-card.component.ts
+++ src/app/modules/commons/event-card/event-card.component.ts
@@ -7,12 +7,12 @@ import { Evento } from './events.model';
   styleUrls: ['./event-card.component.scss']
 })
 export class EventCardComponent {
-  evento1 = new Evento(1, "Carrera Popular", "Carrera popular gratuita de Bormujos", "Bormujos", new Date(2023,2,20), 0, "../../../../assets/img/Runner.jpg" )
-  evento2 = new Evento(2, "Media Maratón", "Media maratón en Camas", "Camas", new Date(2023,3,21), 7, "../../../../assets/img/Camas.jpg" )
-  evento3 = new Evento(3, "Carrera Nocturna", "Carrera nocturna por Espartinas", "Espartinas", new Date(2023,4,22), 15, "../../../../assets/img/espartinas.jpg" )
-  evento4 = new Evento(4, "Maratón", "Gran maratón de Gines", "Gines", new Date(2023,5,23), 25, "../../../../assets/img/Gines2.jpg" )
-  evento5 = new Evento(5, "Carrera Popular", "Carrera popular anual de Mairena del Aljarafe", "Mairena del Aljarafe", new Date(2023,6,24), 1, "../../../../assets/img/MairenaDelAljarafe.jpg" )
-  evento6 = new Evento(6, "Media Maratón Junior", "Media maratón junior en Tomares", "Tomares", new Date(2023,7,25), 5, "../../../../assets/img/Tomares.jpg" )
+  evento1 = new Evento(1, "Carrera Popular", "Carrera popular gratuita de Bormujos", "Bormujos", new Date(2023,2,20), 0, "/NexoRun-Proyecto-Running/assets/img/Runner.jpg" )
+  evento2 = new Evento(2, "Media Maratón", "Media maratón en Camas", "Camas", new Date(2023,3,21), 7, "/NexoRun-Proyecto-Running/assets/img/Camas.jpg" )
+  evento3 = new Evento(3, "Carrera Nocturna", "Carrera nocturna por Espartinas", "Espartinas", new Date(2023,4,22), 15, "/NexoRun-Proyecto-Running/assets/img/espartinas.jpg" )
+  evento4 = new Evento(4, "Maratón", "Gran maratón de Gines", "Gines", new Date(2023,5,23), 25, "/NexoRun-Proyecto-Running/assets/img/Gines2.jpg" )
+  evento5 = new Evento(5, "Carrera Popular", "Carrera popular anual de Mairena del Aljarafe", "Mairena del Aljarafe", new Date(2023,6,24), 1, "/NexoRun-Proyecto-Running/assets/img/MairenaDelAljarafe.jpg" )
+  evento6 = new Evento(6, "Media Maratón Junior", "Media maratón junior en Tomares", "Tomares", new Date(2023,7,25), 5, "/NexoRun-Proyecto-Running/assets/img/Tomares.jpg" )
 
   eventos = [this.evento1, this.evento2, this.evento3];
   eventos2= [this.evento4, this.evento5, this.evento6]
