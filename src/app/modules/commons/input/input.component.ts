ee5b5a3ff7f146912f4200af0799ea35b2ca4e51
Antonio Alfonso González Rodríguez <alum.agonzalezr.2021@iesalixar.org>
2023-03-07 18:00:07 +0100

    Revert "quito input"
    This reverts commit 267fd3ead6250903356cb05f042fb19fdbc5669e.

---
+++ src/app/modules/commons/input/input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+var template = ``;
+
+@Component({
+  selector: 'app-input-text',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent {
+}
